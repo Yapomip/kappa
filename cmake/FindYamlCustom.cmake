85a5503529b0290627c4cfef91ccf060df682052
Yapomip <zenovaromir@gmail.com>
2026-02-06 02:27:47 +0300

    Some code decoration

--- cmake/FindYamlCustom.cmake
+++ cmake/FindYamlCustom.cmake
@@ -1,13 +1,13 @@
 
 find_package(yaml-cpp QUIET PATHS ${yaml-cpp_DIR})
 if(NOT yaml-cpp_FOUND)
-   set(YAML_CPP_INSTALL ON CACHE BOOL "Enable installation of yaml-cpp targets" FORCE)
+    set(YAML_CPP_INSTALL ON CACHE BOOL "Enable installation of yaml-cpp targets" FORCE)
 
-   include(FetchContent)
-   FetchContent_Declare(
-      yaml-cpp
-      GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
-      GIT_TAG f0329d0efd2e62ceda5ee899c343ee445f6f7bd2
-   )
-   FetchContent_MakeAvailable(yaml-cpp)
+    include(FetchContent)
+    FetchContent_Declare(
+        yaml-cpp
+        GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
+        GIT_TAG f0329d0efd2e62ceda5ee899c343ee445f6f7bd2
+    )
+    FetchContent_MakeAvailable(yaml-cpp)
 endif()
